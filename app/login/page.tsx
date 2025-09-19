bf8f3cacbbe248791c13d4033720995601d88145
chanuuuuu <dn0208@gmail.com>
2025-09-19 19:09:25 +0900

    refactor | client 컴포넌트 변경으로 인한 param 제거

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,15 +1,10 @@
 import LoginForm from "../component/LoginForm";
 import styles from "../page.module.css";
-export default async function LoginPage({
-  searchParams,
-}: {
-  searchParams: Promise<{ errorCode: string }>;
-}) {
-  const { errorCode } = await searchParams;
+export default async function LoginPage() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <LoginForm errorCode={errorCode} />
+        <LoginForm />
       </main>
     </div>
   );
